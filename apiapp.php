0d74688aef8fe0067c0a65f9191bce2ae66372f2
ismail <andaloussiismail@gmail.com>
2022-03-01 23:29:57 +0100

    Update apiapp.php

--- apiapp.php
+++ apiapp.php
@@ -13,7 +13,7 @@
             "FaceLite": "269008118",
             "Insta": "20900027",
             "InstaLite": "2670015118",
-            "Wts": "2211817\n  ",
+            "Wts": "2211817",
             "share": "sadvf",
             "AllData": "{\"limitProject\":{\"ads_activated\":1,\"clicks\":1,\"ban_hours\":12,\"delay_seconds\":120,\"ads\":[{\"type\":\"interstitial\",\"admob_id\":\"\",\"name\":\"interstitial_1\"}]},\"status\":true}\n",
             "paack": "com.zhiliaoapp.musically---100@@com.zhiliaoapp.musically.go---100@@com.snapchat.android---100@@com.facebook.orca---100@@com.facebook.mlite---100@@org.telegram.messenger---100@@com.dts.freefireth---100@@com.tencent.ig---100@@com.tencent.iglite---100@@com.facebook.katana---3370032118@@com.facebook.mlite---269008118@@com.instagram.android---20900027@@com.instagram.lite---2670015118@@com.whatsapp---2211817",
