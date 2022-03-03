9b8e491204586c432cc1e5c1da7cab533c24cfc2
ismail <andaloussiismail@gmail.com>
2022-03-03 13:15:48 +0100

    Create playapi.php

---
+++ playapi.php
@@ -0,0 +1,17 @@
+{
+  "LIVETV": [
+    {
+      "update": "now",
+      "UpdateInStoreOrServer": "Store",
+      "linkAppDownload": "https://captainshoot.com/",
+      "linkAppUpdate": "https://captainshoot.com/",
+      "Publisherid": "",
+      "Intersitial": "ca-app-pub-3940256099942544/1033173712",
+      "Banner": "",
+      "Player": "",
+      "iron": "125b0213d",
+      "useragent": "Mozilla/5.0 (iPad; CPU OS 12_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/97.0 Mobile/15E148 Safari/605.1.15",
+      "AllData": "{\"limitProject\":{\"ads_activated\":1,\"clicks\":1,\"ban_hours\":12,\"delay_seconds\":300,\"ads\":[{\"type\":\"interstitial\",\"admob_id\":\"ca-app-pub-4320839425986637/4133286789\",\"name\":\"interstitial_1\"}]},\"status\":true}\n"
+    }
+  ]
+}
