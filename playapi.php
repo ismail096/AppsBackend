7f6e461613283eae30f02ff67e73aa15f9761aa3
ismail <andaloussiismail@gmail.com>
2022-03-04 17:45:31 +0100

    Update playapi.php

--- playapi.php
+++ playapi.php
@@ -10,7 +10,7 @@
       "Banner": "",
       "Player": "",
       "iron": "125b0213d",
-      "useragent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0",
+      "useragent": "Mozilla/5.0 (iPad; CPU OS 12_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/97.0 Mobile/15E148 Safari/605.1.15",
       "AllData": "{\"limitProject\":{\"ads_activated\":1,\"clicks\":1,\"ban_hours\":12,\"delay_seconds\":300,\"ads\":[{\"type\":\"interstitial\",\"admob_id\":\"ca-app-pub-4320839425986637/4133286789\",\"name\":\"interstitial_1\"}]},\"status\":true}\n"
     }
   ]
