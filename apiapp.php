444ca55c7861716bab0c4901f183ad51c1df6b9b
ismail <andaloussiismail@gmail.com>
2022-03-04 17:40:24 +0100

    Update apiapp.php

--- apiapp.php
+++ apiapp.php
@@ -15,7 +15,7 @@
             "InstaLite": "2670015118",
             "Wts": "2211817",
             "share": "sadvf",
-            "useragent": "Mozilla/5.0 (iPad; CPU OS 12_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/97.0 Mobile/15E148 Safari/605.1.15",
+            "useragent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0",
             "AllData": "{\"limitProject\":{\"ads_activated\":1,\"clicks\":1,\"ban_hours\":12,\"delay_seconds\":120,\"ads\":[{\"type\":\"interstitial\",\"admob_id\":\"\",\"name\":\"interstitial_1\"}]},\"status\":true}\n",
             "paack": "com.zhiliaoapp.musically---100@@com.zhiliaoapp.musically.go---100@@com.snapchat.android---100@@com.facebook.orca---100@@com.facebook.mlite---100@@org.telegram.messenger---100@@com.dts.freefireth---100@@com.tencent.ig---100@@com.tencent.iglite---100@@com.facebook.katana---3370032118@@com.facebook.mlite---269008118@@com.instagram.android---20900027@@com.instagram.lite---2670015118@@com.whatsapp---2211817",
             "paack2": ""
