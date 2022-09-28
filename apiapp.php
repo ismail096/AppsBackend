fa50caffac8a050775c1af367976bffcd4b545eb
ismail <andaloussiismail@gmail.com>
2022-09-28 23:44:42 +0100

    Update apiapp.php

--- apiapp.php
+++ apiapp.php
@@ -17,7 +17,7 @@
             "share": "sadvf",
             "useragent": "Mozilla/5.0 (iPad; CPU OS 12_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/97.0 Mobile/15E148 Safari/605.1.15",
             "AllData": "{\"limitProject\":{\"ads_activated\":1,\"clicks\":1,\"ban_hours\":12,\"delay_seconds\":120,\"ads\":[{\"type\":\"interstitial\",\"admob_id\":\"\",\"name\":\"interstitial_1\"}]},\"status\":true}\n",
-            "valueMainLib": "1"
+            "valueMainLib": "0"
 
         }
     ]
